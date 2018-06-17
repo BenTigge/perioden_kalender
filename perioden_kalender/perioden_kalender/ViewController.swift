b7e2bd3da6018a28d4f2a82479a05a3e3d7fbe00
Benjamin Tiggemann <benjamin.tiggemann@gmx.net>
2018-06-17 22:54:39 +0200

    new empty swift project

---
+++ perioden_kalender/perioden_kalender/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  perioden_kalender
+//
+//  Created by Benjamin Tiggemann on 17.06.18.
+//  Copyright © 2018 Benjamin Tiggemann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
