84bd10a4660f03b521ead85500befffd46da2065
Benjamin Tiggemann <benjamin.tiggemann@gmx.net>
2018-06-20 20:41:26 +0200

    new empty ViewControllers

---
+++ myCalendar/myCalendar/ForYouViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ForYouViewController.swift
+//  myCalendar
+//
+//  Created by Benjamin Tiggemann on 20.06.18.
+//  Copyright © 2018 Benjamin Tiggemann. All rights reserved.
+//
+
+import UIKit
+
+class ForYouViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ myCalendar/myCalendar/SettingsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsViewController.swift
+//  myCalendar
+//
+//  Created by Benjamin Tiggemann on 20.06.18.
+//  Copyright © 2018 Benjamin Tiggemann. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
